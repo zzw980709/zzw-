45f957b6ff4879f0699d85f81605a675ca5cb3d5
zhengzhiwei <18783837850@163.com>
2020-02-08 11:31:43 +0800

    提交

---
+++ fibnaci.py
@@ -0,0 +1,11 @@
+# 斐波那契数列生成函数
+def fib(n):
+    a,b = 0,1
+    for _ in range(n):
+        a,b = b,a + b
+        yield a
+def main():
+    for val in fib(20):
+        print(val)
+if __name__ == '__main__':
+    main()
